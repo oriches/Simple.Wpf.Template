ce06764ccee858e41b1603a0fdd19445fdc999b9
Ollie Riches <ollie.riches@btinternet.com>
2014-09-23 15:20:13 +0100

    Create MainWindow.xaml.cs

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+namespace WpfTemplate.Views
+{
+    using System.Windows;
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
