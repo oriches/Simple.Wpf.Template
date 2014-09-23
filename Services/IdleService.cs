c2bb8e7fb14b00af85c8ceeb949da40caeb87a7d
Ollie Riches <ollie.riches@btinternet.com>
2014-09-23 15:09:18 +0100

    Create IdleService.cs

---
+++ Services/IdleService.cs
@@ -0,0 +1,43 @@
+namespace WpfTemplate
+{
+    using System;
+    using System.Reactive;
+    using System.Reactive.Disposables;
+    using System.Reactive.Linq;
+    using System.Reactive.Subjects;
+    using System.Windows;
+    using NLog;
+
+    public sealed class IdleService : IIdleService, IDisposable
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private readonly IConnectableObservable<EventPattern<EventArgs>> _idleObservable;
+        private readonly IDisposable _disposable;
+
+        public IdleService(ISchedulerService schedulerService)
+        {
+            var mainWindow = Application.Current.MainWindow;
+
+            _idleObservable = Observable.FromEventPattern(h => mainWindow.Dispatcher.Hooks.DispatcherInactive += h,
+                h => mainWindow.Dispatcher.Hooks.DispatcherInactive -= h, schedulerService.TaskPool)
+                .Publish();
+
+            _disposable = _idleObservable.Connect();
+            _disposable = Disposable.Empty;
+        }
+
+        public IObservable<Unit> Idling()
+        {
+            return _idleObservable.Select(x => Unit.Default);
+        }
+
+        public void Dispose()
+        {
+            using (Duration.Measure(Logger, "Dispose"))
+            {
+                _disposable.Dispose();
+            }
+        }
+    }
+}
