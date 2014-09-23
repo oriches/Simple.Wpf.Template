d499f0c27995a4cf7a21e1b052b4ae906a41bd36
Ollie Riches <ollie.riches@btinternet.com>
2014-09-23 15:10:58 +0100

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,14 @@
+namespace WpfTemplate
+{
+    using System;
+
+    public static class Constants
+    {
+        public static readonly TimeSpan Heartbeat = TimeSpan.FromSeconds(60);
+        public static readonly TimeSpan UiFreeze = TimeSpan.FromMilliseconds(750);
+
+        public static readonly TimeSpan DiagnosticsIdleBuffer = TimeSpan.FromMilliseconds(999);
+        public static readonly TimeSpan DiagnosticsCpuBuffer = TimeSpan.FromMilliseconds(750);
+        public static readonly TimeSpan DiagnosticsSubscriptionDelay = TimeSpan.FromMilliseconds(7500);
+    }
+}
