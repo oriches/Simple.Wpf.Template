2a436bd093549a50a605b7cd569e97947deb49cd
Ollie Riches <ollie.riches@btinternet.com>
2014-09-24 15:54:33 +0100

    Update Child2ViewModel.cs

--- ViewModels/Child2ViewModel.cs
+++ ViewModels/Child2ViewModel.cs
@@ -1,7 +1,22 @@
 namespace WpfTemplate.ViewModels
 {
+    using System.Windows.Input;
+    using Commands;
+
     public sealed class Child2ViewModel : BaseViewModel
     {
+        public Child2ViewModel()
+        {
+            DelayCommand = new RelayCommand(Delay);
+        }
+
         public string Title { get { return "Child 2"; } }
+
+        public ICommand DelayCommand { get; private set; }
+
+        private static void Delay()
+        {
+            System.Threading.Thread.Sleep(3123);
+        }
     }
 }
