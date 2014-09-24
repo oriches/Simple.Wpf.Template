b5b161be598d130067f7654e73aad3e3152dd9d8
Ollie Riches <ollie.riches@btinternet.com>
2014-09-24 16:12:36 +0100

    Update Child2ViewModel.cs

--- ViewModels/Child2ViewModel.cs
+++ ViewModels/Child2ViewModel.cs
@@ -2,21 +2,21 @@ namespace WpfTemplate.ViewModels
 {
     using System.Windows.Input;
     using Commands;
+    using Services;
 
     public sealed class Child2ViewModel : BaseViewModel
     {
-        public Child2ViewModel()
+        public Child2ViewModel(IGestureService gestureService)
         {
-            DelayCommand = new RelayCommand(Delay);
+            DelayCommand = new RelayCommand(() =>
+            {
+                gestureService.SetBusy();
+                System.Threading.Thread.Sleep(3123);
+            });
         }
 
         public string Title { get { return "Child 2"; } }
 
         public ICommand DelayCommand { get; private set; }
-
-        private static void Delay()
-        {
-            System.Threading.Thread.Sleep(3123);
-        }
     }
 }
