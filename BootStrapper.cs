c8125680a123ee5f0ab122f121dee45d4006687d
Ollie Riches <ollie.riches@btinternet.com>
2014-09-24 12:14:40 +0100

    Update BootStrapper.cs

--- BootStrapper.cs
+++ BootStrapper.cs
@@ -42,6 +42,8 @@ namespace WpfTemplate
             
             builder.RegisterAssemblyTypes(assembly)
                 .Where(t => t.Name.EndsWith("ViewModel"));
+
+            builder.RegisterType<Heartbeat>();
             
             _rootScope = builder.Build();
         }
