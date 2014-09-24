5381af2e189fa7678eb912fffaba99a7b9e66ba8
Ollie Riches <ollie.riches@btinternet.com>
2014-09-24 15:57:15 +0100

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -5,7 +5,8 @@ namespace WpfTemplate
     public static class Constants
     {
         public static readonly TimeSpan Heartbeat = TimeSpan.FromSeconds(60);
-        public static readonly TimeSpan UiFreeze = TimeSpan.FromMilliseconds(750);
+        public static readonly TimeSpan UiFreeze = TimeSpan.FromMilliseconds(500);
+        public static readonly TimeSpan UiFreezeTimer = TimeSpan.FromMilliseconds(333);
 
         public static readonly TimeSpan DiagnosticsIdleBuffer = TimeSpan.FromMilliseconds(999);
         public static readonly TimeSpan DiagnosticsCpuBuffer = TimeSpan.FromMilliseconds(750);
