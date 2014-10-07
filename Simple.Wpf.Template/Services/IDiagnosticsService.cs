e49598ada6b14562fcc41f3e5dbdee604951b681
Ollie Riches <ollie.riches@btinternet.com>
2014-10-07 14:17:27 +0100

    Update IDiagnosticsService.cs

--- Simple.Wpf.Template/Services/IDiagnosticsService.cs
+++ Simple.Wpf.Template/Services/IDiagnosticsService.cs
@@ -1,15 +1,14 @@
 namespace Simple.Wpf.Template.Services
 {
     using System;
-    using System.Reactive;
     using Models;
 
     public interface IDiagnosticsService
     {
-        IObservable<Unit> Initialised { get; }
-
         IObservable<Memory> Memory { get; }
 
-        IObservable<int> CpuUtilisation { get; }
+        IObservable<int> Cpu { get; }
+
+        IObservable<int> Fps { get; }
     }
 }
