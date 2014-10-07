d62f8d12cad968dd3bc201b44f07d36d5c107209
Ollie Riches <ollie.riches@btinternet.com>
2014-10-07 14:19:10 +0100

    Update DiagnosticsViewModelFixtures.cs

--- Simple.Wpf.Template.Tests/DiagnosticsViewModelFixtures.cs
+++ Simple.Wpf.Template.Tests/DiagnosticsViewModelFixtures.cs
@@ -15,8 +15,9 @@ namespace Simple.Wpf.Template.Tests
         private TestScheduler _testScheduler;
         private MockSchedulerService _schedulerService;
         private Mock<IDiagnosticsService> _diagnosticService;
-        private Subject<int> _cpuUtilisationSubject;
+        private Subject<int> _cpuSubject;
         private Subject<Memory> _memorySubject;
+        private Subject<int> _fpsSubject;
 
         [SetUp]
         public void SetUp()
@@ -26,11 +27,25 @@ namespace Simple.Wpf.Template.Tests
 
             _diagnosticService = new Mock<IDiagnosticsService>(MockBehavior.Strict);
 
-            _cpuUtilisationSubject = new Subject<int>();
-            _diagnosticService.Setup(x => x.CpuUtilisation).Returns(_cpuUtilisationSubject);
+            _fpsSubject = new Subject<int>();
+            _diagnosticService.Setup(x => x.Fps).Returns(_fpsSubject);
+
+            _cpuSubject = new Subject<int>();
+            _diagnosticService.Setup(x => x.Cpu).Returns(_cpuSubject);
 
             _memorySubject = new Subject<Memory>();
             _diagnosticService.Setup(x => x.Memory).Returns(_memorySubject);
+        }
+
+        [Test]
+        public void when_created_fps_is_default_value()
+        {
+            // ARRANGE
+            var viewModel = new DiagnosticsViewModel(_diagnosticService.Object, _schedulerService);
+
+            // ACT
+            // ASSERT
+            Assert.That(viewModel.Fps, Is.EqualTo(Constants.DefaultFpsString));
         }
 
         [Test]
@@ -67,28 +82,58 @@ namespace Simple.Wpf.Template.Tests
         }
 
         [Test]
-        public void cpu_value_is_formatted_when_diagnostics_service_pumps_cpu_utilisation()
+        public void fps_value_is_formatted_when_diagnostics_service_pumps_fps()
         {
             // ARRANGE
             var viewModel = new DiagnosticsViewModel(_diagnosticService.Object, _schedulerService);
 
             // ACT
-            _cpuUtilisationSubject.OnNext(42);
+            _fpsSubject.OnNext(66);
+
+            _testScheduler.AdvanceBy(TimeSpan.FromSeconds(2));
+
+            // ASSERT
+            Assert.That(viewModel.Fps, Is.EqualTo("Render: 66 FPS"));
+        }
+
+        [Test]
+        public void fps_value_is_default_value_when_diagnostics_service_fps_errors()
+        {
+            // ARRANGE
+            var viewModel = new DiagnosticsViewModel(_diagnosticService.Object, _schedulerService);
+
+            // ACT
+            _fpsSubject.OnError(new Exception("blah!"));
+
+            _testScheduler.AdvanceBy(TimeSpan.FromSeconds(1));
+
+            // ASSERT
+            Assert.That(viewModel.Fps, Is.EqualTo(Constants.DefaultFpsString));
+        }
+
+        [Test]
+        public void cpu_value_is_formatted_when_diagnostics_service_pumps_cpu()
+        {
+            // ARRANGE
+            var viewModel = new DiagnosticsViewModel(_diagnosticService.Object, _schedulerService);
+
+            // ACT
+            _cpuSubject.OnNext(42);
 
             _testScheduler.AdvanceBy(TimeSpan.FromSeconds(1));
             
             // ASSERT
-            Assert.That(viewModel.Cpu, Is.EqualTo("CPU: 42%"));
+            Assert.That(viewModel.Cpu, Is.EqualTo("CPU: 42 %"));
         }
 
         [Test]
-        public void cpu_value_is_default_value_when_diagnostics_service_cpu_utilisation_errors()
+        public void cpu_value_is_default_value_when_diagnostics_service_cpu_errors()
         {
             // ARRANGE
             var viewModel = new DiagnosticsViewModel(_diagnosticService.Object, _schedulerService);
 
             // ACT
-            _cpuUtilisationSubject.OnError(new Exception("blah!"));
+            _cpuSubject.OnError(new Exception("blah!"));
 
             _testScheduler.AdvanceBy(TimeSpan.FromSeconds(1));
 
@@ -175,9 +220,11 @@ namespace Simple.Wpf.Template.Tests
             viewModel.Dispose();
 
             _memorySubject.OnNext(new Memory(totalMemory, managedMemory));
-            _cpuUtilisationSubject.OnNext(42);
+            _cpuSubject.OnNext(42);
+            _fpsSubject.OnNext(65);
 
             // ASSERT
+            Assert.That(viewModel.Fps, Is.EqualTo(Constants.DefaultFpsString));
             Assert.That(viewModel.Cpu, Is.EqualTo(Constants.DefaultCpuString));
             Assert.That(viewModel.TotalMemory, Is.EqualTo(Constants.DefaultTotalMemoryString));
             Assert.That(viewModel.ManagedMemory, Is.EqualTo(Constants.DefaultManagedMemoryString));
