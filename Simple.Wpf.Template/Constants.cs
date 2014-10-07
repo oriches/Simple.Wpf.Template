625e56b243a14beb1e083725a046504b9db56d7c
Ollie Riches <ollie.riches@btinternet.com>
2014-10-07 14:15:45 +0100

    Update Constants.cs

--- Simple.Wpf.Template/Constants.cs
+++ Simple.Wpf.Template/Constants.cs
@@ -12,7 +12,8 @@ namespace Simple.Wpf.Template
         public static readonly TimeSpan DiagnosticsCpuBuffer = TimeSpan.FromMilliseconds(666);
         public static readonly TimeSpan DiagnosticsSubscriptionDelay = TimeSpan.FromMilliseconds(7500);
 
-        public const string DefaultCpuString = "CPU: 00%";
+        public const string DefaultFpsString = "Render: 00 FPS";
+        public const string DefaultCpuString = "CPU: 00 %";
         public const string DefaultManagedMemoryString = "Managed Memory: 00 Mb";
         public const string DefaultTotalMemoryString = "Total Memory: 00 Mb";
     }
